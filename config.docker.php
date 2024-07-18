f5644a2b38a57d37752e9e4adbea0be4829d8ab5
Orsokuma <magictallguy@hotmail.com>
2024-07-18 16:32:37 +0100

    Fernando's Docker, modified

---
+++ config.docker.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+// Consider Dotenv/Dotenv
+$env_file      = [];
+$env_file_path = __DIR__ . '/.env';
+if (file_exists($env_file_path)) {
+    $env_file = parse_ini_file($env_file_path);
+}
+
+/**
+ * @param string $name
+ * @return string
+ */
+function read_secret(string $name): string
+{
+    if ($value = getenv($name . '_FILE')) {
+        return trim(file_get_contents($value));
+    }
+    if ($value = getenv($name)) {
+        return trim($value);
+    }
+    return '';
+}
+
+$_CONFIG = [
+    'hostname' => read_secret('DB_HOST'),
+    'username' => read_secret('DB_USER'),
+    'password' => read_secret('DB_PASS'),
+    'database' => read_secret('DB_NAME'),
+    'code' => read_secret('APP_KEY'),
+];
