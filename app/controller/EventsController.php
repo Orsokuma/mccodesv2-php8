d2bf26b048477fa7b64a01a73254eada50c5deb5
Orsokuma <magictallguy@hotmail.com>
2024-07-21 04:01:26 +0100

    Refactor events

--- app/controller/EventsController.php
+++ app/controller/EventsController.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+use ParagonIE\EasyDB\EasyPlaceholder;
+
 if (!defined('MONO_ON') || !MONO_ON) {
     exit;
 }
@@ -12,7 +14,12 @@ class EventsController extends CommonObjects
 {
     private const string EVENTS_DELETED = 'All %s of your events have been deleted.';
 
-    protected function deleteOne(?int $id): array
+    /**
+     * @param int|null $id
+     * @return array
+     * @throws Throwable
+     */
+    protected function doDeleteOne(?int $id): array
     {
         if (empty($id)) {
             return [
@@ -20,29 +27,45 @@ class EventsController extends CommonObjects
                 'message' => sprintf(self::MISSED_REQUIRED_OBJECT, 'event'),
             ];
         }
-        $recipient = $this->pdo->cell(
-            'SELECT evUSER FROM events WHERE evID = ? AND evUSER = ?',
+        $row = $this->pdo->row(
+            'SELECT evID, evUSER, evREAD FROM events WHERE evID = ? AND evUSER = ?',
             $id, $this->player['userid'],
         );
-        if (!$recipient) {
+        if (empty($row)) {
             return [
                 'type' => 'error',
-                'message' => sprintf(self::NO_EXIST_OR_NOT_YOURS, 'event'),
+                'message' => sprintf(self::NOT_EXIST_OR_NOT_YOURS, 'event'),
             ];
         }
-        $this->pdo->delete(
-            'events',
-            [
-                'evID' => $id,
-                'evUSER' => $this->player['userid'],
-            ]
-        );
+        $save = function () use ($row) {
+            $this->pdo->delete(
+                'events',
+                [
+                    'evID' => $row['evID'],
+                    'evUSER' => $this->player['userid'],
+                ]
+            );
+            if (!$row['evREAD']) {
+                $this->pdo->update(
+                    'users',
+                    ['new_events' => new EasyPlaceholder('GREATEST(new_events - 1, 0)')],
+                    ['userid' => $this->player['userid']],
+                );
+            }
+        };
+        $this->pdo->tryFlatTransaction($save);
+
         return [
             'type' => 'success',
             'message' => sprintf(self::OBJECT_DELETED, 'event'),
         ];
     }
-    protected function deleteAll(): array
+
+    /**
+     * @return array
+     * @throws Throwable
+     */
+    protected function doDeleteAll(): array
     {
         $count = $this->pdo->cell(
             'SELECT COUNT(*) FROM events WHERE evUSER = ?',
@@ -54,10 +77,21 @@ class EventsController extends CommonObjects
                 'message' => sprintf(self::NO_OBJECT_TO_DELETE, 'events'),
             ];
         }
-        $deleted = $this->pdo->delete(
-            'events',
-            ['evUSER' => $this->player['userid']],
-        );
+        $deleted = 0;
+        $save = function () use ($count, &$deleted) {
+            $deleted = $this->pdo->delete(
+                'events',
+                ['evUSER' => $this->player['userid']],
+            );
+            if ($this->player['new_events']) {
+                $this->pdo->update(
+                    'users',
+                    ['new_events' => 0],
+                    ['userid' => $this->player['userid']],
+                );
+            }
+        };
+        $this->pdo->tryFlatTransaction($save);
         return [
             'type' => 'success',
             'message' => sprintf(self::EVENTS_DELETED, number_format($deleted)),
--- app/model/EventsHandler.php
+++ app/model/EventsHandler.php
@@ -1,5 +1,9 @@
 <?php
 declare(strict_types=1);
+
+use ParagonIE\EasyDB\EasyPlaceholder;
+use ParagonIE\EasyDB\EasyStatement;
+
 if (!defined('MONO_ON') || !MONO_ON) {
     exit;
 }
@@ -13,24 +17,90 @@ class EventsHandler extends EventsController
      * @param string|null $subRoute
      * @param string|null $id
      * @return void
-     * @throws Exception
+     * @throws Throwable
      */
     public function get(?string $subRoute = null, ?string $id = null): void
     {
         if ($id !== null) {
             $id = (int)$id;
         }
+        $page_num = is_numeric($subRoute) ? (int)$subRoute : 1;
         match ($subRoute) {
             'delete' => $this->displayDeleteOneConfirmation($id),
             'delete-all' => $this->displayDeleteAllConfirmation(),
-            default => $this->displayEvents((int)$subRoute),
+            'send-test-event' => $this->sendTestEvent($id),
+            'reset-unread-event-counter' => $this->resetUnreadEventCounter(),
+            default => $this->displayEvents($page_num),
         };
+    }
+
+    /**
+     * @param int|null $id
+     * @return void
+     */
+    private function displayDeleteOneConfirmation(?int $id): void
+    {
+        $template = file_get_contents($this->view . '/template/confirm-action.html');
+        echo strtr($template, [
+            '{{ACTION}}' => '/events/delete/' . $id,
+            '{{CONTENT}}' => 'delete this event?',
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    private function displayDeleteAllConfirmation(): void
+    {
+        $template = file_get_contents($this->view . '/template/confirm-action.html');
+        echo strtr($template, [
+            '{{ACTION}}' => '/events/delete-all',
+            '{{CONTENT}}' => 'delete all events?',
+        ]);
+    }
+
+    /**
+     * @param int $times
+     * @return void
+     */
+    private function sendTestEvent(int $times = 1): void
+    {
+        $times = min(250, max(1, $times));
+        for ($i = 0; $i < $times; ++$i) {
+            $this->func->event_add($this->player['userid'], 'Test event!');
+        }
+        $_SESSION['success'] = $times.' test event'.($times === 1 ? '' : 's').' sent.';
+        header('Location: /events');
+        exit;
+    }
+
+    /**
+     * @return void
+     */
+    private function resetUnreadEventCounter(): void
+    {
+        $unread_count = $this->pdo->cell(
+            'SELECT COUNT(*) FROM events WHERE evUSER = ? AND evREAD = 0',
+            $this->player['userid'],
+        );
+        if ($unread_count === $this->player['new_events']) {
+            $_SESSION['info'] = 'Your unread event count is correct.';
+        } else {
+            $this->pdo->update(
+                'users',
+                ['new_events' => $unread_count],
+                ['userid' => $this->player['userid']],
+            );
+            $_SESSION['success'] = 'Your unread event count has been updated.';
+        }
+        header('Location: /events');
+        exit;
     }
 
     /**
      * @param int $pageNum
      * @return void
-     * @throws Exception
+     * @throws Throwable
      */
     private function displayEvents(int $pageNum = 1): void
     {
@@ -40,6 +110,10 @@ class EventsHandler extends EventsController
         );
         $pages    = new Pagination($count, $pageNum, '/events/(:num)');
         $template = file_get_contents($this->view . '/auth/events/index.html');
+        preg_match('/\{{2}IF:EVENT_COUNT}{2}(.+?)\{{2}ENDIF}{2}/s', $template, $matches);
+        $template = $count > 0
+            ? str_replace($matches[0], $matches[1], $template)
+            : str_replace($matches[0], '', $template);
         echo strtr($template, [
             '{{PAGINATION}}' => $pages->toHtml(),
             '{{EVENTS}}' => $this->renderEventContent($pages),
@@ -49,7 +123,7 @@ class EventsHandler extends EventsController
     /**
      * @param Pagination $pages
      * @return string
-     * @throws Exception
+     * @throws Throwable
      */
     private function renderEventContent(Pagination $pages): string
     {
@@ -59,14 +133,77 @@ class EventsHandler extends EventsController
             $this->player['userid'],
         );
         $content        = '';
+        $unread         = [];
         foreach ($rows as $row) {
-            $date    = new \DateTime('@' . $row['evTIME']);
+            if (!$row['evREAD']) {
+                $unread[] = $row['evID'];
+            }
+            $date    = new DateTime('@' . $row['evTIME']);
             $content .= strtr($entry_template, [
                 '{{ID}}' => $row['evID'],
+                '{{NEW}}' => !$row['evREAD'] ? '<br><strong>New!</strong>' : '',
                 '{{DATE}}' => $date->format($this->siteSettings['default_date_format']),
                 '{{EVENT}}' => nl2br($row['evTEXT']),
             ]);
         }
+        $this->unflagRead($unread);
         return $content;
+    }
+
+    /**
+     * @param array $unread
+     * @return void
+     * @throws Throwable
+     */
+    private function unflagRead(array $unread): void
+    {
+        $count = count($unread);
+        if (!$count) {
+            return;
+        }
+        $save = function () use ($unread, $count) {
+            $statement = EasyStatement::open()
+                ->in('evID IN (?*)', $unread);
+            $this->pdo->safeQuery(
+                'UPDATE events SET evREAD = 1 WHERE ' . $statement,
+                $statement->values()
+            );
+            $this->pdo->update(
+                'users',
+                ['new_events' => new EasyPlaceholder('new_events - ?', $count)],
+                ['userid' => $this->player['userid']],
+            );
+        };
+        $this->pdo->tryFlatTransaction($save);
+    }
+
+    /**
+     * @param string|null $subRoute
+     * @param string|null $id
+     * @return void
+     * @throws Throwable
+     */
+    public function post(?string $subRoute = null, ?string $id = null): void
+    {
+        $this->afterPost($this->handlePost($subRoute, $id), 'events');
+    }
+
+    /**
+     * @param string|null $subRoute
+     * @param string|null $id
+     * @return array
+     * @throws Throwable
+     */
+    private function handlePost(?string $subRoute = null, ?string $id = null): array
+    {
+        $response = match ($subRoute) {
+            'delete-all' => $this->doDeleteAll(),
+            'delete' => $this->doDeleteOne((int)$id),
+            default => [],
+        };
+        if (empty($response)) {
+            ToroHook::fire('404');
+        }
+        return $response;
     }
 }
